b9e7c533e4c1e02bd9aad97e4a5004c65bb73c7c
Avinash Tingre <avinash.tingre@cerence.com>
2021-09-15 17:33:06 +0530

    Enable sending mail with attachment + code refactor

--- src/main/java/com/avinash/jobreferral/services/MailServiceImpl.java
+++ src/main/java/com/avinash/jobreferral/services/MailServiceImpl.java
@@ -2,9 +2,11 @@ package com.avinash.jobreferral.services;
 
 import com.avinash.jobreferral.bean.Contact;
 import com.avinash.jobreferral.bean.Mail;
+import com.avinash.jobreferral.configuration.ConfigProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
@@ -12,14 +14,10 @@ import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
+import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 @Service("mailService")
 public class MailServiceImpl implements MailService {
@@ -31,44 +29,57 @@ public class MailServiceImpl implements MailService {
     @Autowired
     FileService fileService;
 
-    Queue<Contact> emailQueue = new PriorityQueue<>();
+    @Autowired
+    ConfigProperties myConfig;
+
+    private static final String MAIL_SENT = "YES";
+    private static final String MAIL_NOT_SENT = "NO";
+
+    private static final List<Contact> successfulDeliveries = new ArrayList<>();
+    private static final List<Contact> failedDeliveries = new ArrayList<>();
+
+    public List<Contact> getSuccessfulDeliveries() {
+        return successfulDeliveries;
+    }
+
+    public List<Contact> getFailedDeliveries() {
+        return failedDeliveries;
+    }
 
     public void prepareAndSendMail() throws IOException {
+        Queue<Contact> emailQueue = new PriorityQueue<>();
+
         List<Contact> records = fileService.readContactsFromFile();
         List<Contact> finalRecords = new ArrayList<>();
 
         for (Contact contact : records) {
 
-            if(!contact.getAlreadySent().equalsIgnoreCase("Yes")){
+            if (!contact.getAlreadySent().equalsIgnoreCase(MAIL_SENT)) {
                 emailQueue.add(contact);
-            }
-            else{
+            } else {
                 finalRecords.add(contact);
             }
         }
-        if(emailQueue.isEmpty()){
-            LOG.info("No new contacts present in file.");
+        if (emailQueue.isEmpty()) {
+            LOG.info("No new contacts found.");
         }
 
         while (!emailQueue.isEmpty()) {
             Contact contact = emailQueue.poll();
             Mail mail = new Mail();
-            mail.setMailFrom("tingre.avinash@gmail.com");
-            mail.setMailSubject("[REFERRAL REQUEST] Software Engineer | Java");
-            mail.setMailTo(contact.getEmail());
 
-            String content = fileService.formatEmailTemplate(contact);
-            mail.setMailContent(content);
+            mail.setMailTo(contact.getEmail());
+            mail.setMailContent(fileService.customizeMailContent(contact));
 
             try {
                 sendMail(mail);
-                contact.setAlreadySent("Yes");
-                LOG.info("Mail successfully sent to: " + contact.getEmail());
+                contact.setAlreadySent(MAIL_SENT);
+                successfulDeliveries.add(contact);
             } catch (MessagingException | UnsupportedEncodingException e) {
                 e.printStackTrace();
-                LOG.error("Mail sending failed to: "+contact.getEmail());
-                contact.setAlreadySent("No");
-            }finally {
+                contact.setAlreadySent(MAIL_NOT_SENT);
+                failedDeliveries.add(contact);
+            } finally {
                 finalRecords.add(contact);
             }
 
@@ -82,12 +93,15 @@ public class MailServiceImpl implements MailService {
         MimeMessage mimeMessage = mailSender.createMimeMessage();
         MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true);
 
-        mimeMessageHelper.setSubject(mail.getMailSubject());
+        mimeMessageHelper.setSubject(myConfig.getSubject());
+        mimeMessageHelper.setFrom(new InternetAddress(myConfig.getFromAddress(), myConfig.getFromName()));
+
         mimeMessageHelper.setTo(mail.getMailTo());
-        mimeMessageHelper.setFrom(new InternetAddress(mail.getMailFrom(), "Avinash Tingre"));
-        mimeMessageHelper.setText(mail.getMailContent());
+        mimeMessageHelper.setText(mail.getMailContent(), true);
 
-        mailSender.send(mimeMessageHelper.getMimeMessage());
+        FileSystemResource resource = new FileSystemResource(new File(myConfig.getAttachment()));
+        mimeMessageHelper.addAttachment(Objects.requireNonNull(resource.getFilename()), resource);
 
+        //mailSender.send(mimeMessageHelper.getMimeMessage());
     }
 }
