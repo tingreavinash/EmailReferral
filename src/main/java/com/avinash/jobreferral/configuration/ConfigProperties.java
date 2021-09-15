3b5c48383b9855860437c444c891267b24012063
Avinash Tingre <avinash.tingre@cerence.com>
2021-09-15 17:30:45 +0530

    Read properties by getter call + add new properties

---
+++ src/main/java/com/avinash/jobreferral/configuration/ConfigProperties.java
@@ -0,0 +1,63 @@
+package com.avinash.jobreferral.configuration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class ConfigProperties {
+    private String contactLocation;
+    private String templateLocation;
+    private String fromAddress;
+    private String fromName;
+    private String subject;
+    private String attachment;
+
+    public String getContactLocation() {
+        return contactLocation;
+    }
+
+    public void setContactLocation(String contactLocation) {
+        this.contactLocation = contactLocation;
+    }
+
+    public String getTemplateLocation() {
+        return templateLocation;
+    }
+
+    public void setTemplateLocation(String templateLocation) {
+        this.templateLocation = templateLocation;
+    }
+
+    public String getFromAddress() {
+        return fromAddress;
+    }
+
+    public void setFromAddress(String fromAddress) {
+        this.fromAddress = fromAddress;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+}
