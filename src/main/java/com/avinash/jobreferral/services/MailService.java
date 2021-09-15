deae1326a42d18ba0dbe6d3f8e1b910bac892144
Avinash Tingre <avinash.tingre@cerence.com>
2021-09-15 17:33:06 +0530

    Enable sending mail with attachment + code refactor

--- src/main/java/com/avinash/jobreferral/services/MailService.java
+++ src/main/java/com/avinash/jobreferral/services/MailService.java
@@ -1,13 +1,17 @@
 package com.avinash.jobreferral.services;
 
+import com.avinash.jobreferral.bean.Contact;
 import com.avinash.jobreferral.bean.Mail;
 
 import javax.mail.MessagingException;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 public interface MailService {
     void sendMail(Mail mail) throws MessagingException, UnsupportedEncodingException;
 
     void prepareAndSendMail() throws IOException;
+    List<Contact> getSuccessfulDeliveries();
+    List<Contact> getFailedDeliveries();
 }
